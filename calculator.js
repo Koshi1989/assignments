54ef38ab8308c4bbf473aa0e1ee578b7069050b2
Koshi1989 <kmuraokshi@hks.harvard.edu>
2020-01-13 00:30:08 -0500

    modify error of variables

--- calculator.js
+++ calculator.js
@@ -19,11 +19,11 @@ function divide(a,b) {
 
 console.log("I'm going use the calculator functions to multiply 5 and 6")
 var m = multiply(5,6)
-console.log(x)
+console.log(m)
 
 console.log("I'm going use the calculator functions to add 6 and 5")
 var a = add(6,5)
-console.log(y)
+console.log(a)
 
 console.log("I'm going use the calculator functions to subtract 6 and 5")
 var s = subtract(6,5)
@@ -31,4 +31,4 @@ console.log(s)
 
 console.log("I'm going use the calculator functions to divide 6 by 5")
 var d = divide(6,5)
-console.log(z)
+console.log(d)
